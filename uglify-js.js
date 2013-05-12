8e07dc4d270608a6820c7881d3a78656f2a6a1ea
raffecat <atowers@gmail.com>
2013-05-12 12:14:30 +1000

    nodejs extension for .jsr modules

--- uglify-js.js
+++ uglify-js.js
@@ -16,3 +16,29 @@ uglify.uglify = require("./lib/process");
 uglify.consolidator = require("./lib/consolidator");
 
 module.exports = uglify
+
+
+// nodejs require extension for *.jsr files.
+// usage: require('uglify-js');
+
+console.log("installing .jsr extension");
+
+var fs = require('fs');
+require.extensions['.jsr'] = function(module, filename) {
+  console.log("JSR", filename);
+  var content = stripBOM(fs.readFileSync(filename, 'utf8'));
+  var ast = uglify.parser.parse(content, false); // parse (strict_semicolons=false)
+  var code = uglify.uglify.gen_code(ast, {}); // generate code
+  console.log(code);
+  module._compile(code, filename);
+};
+
+function stripBOM(content) {
+  // Remove byte order marker. This catches EF BB BF (the UTF-8 BOM)
+  // because the buffer-to-string conversion in `fs.readFileSync()`
+  // translates it to FEFF, the UTF-16 BOM.
+  if (content.charCodeAt(0) === 0xFEFF) {
+    content = content.slice(1);
+  }
+  return content;
+}
